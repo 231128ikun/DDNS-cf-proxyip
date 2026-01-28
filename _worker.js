7e02b2d0c98a418d65e62a4a822247a058419a1a
glimmer <152295099+231128ikun@users.noreply.github.com>
2026-01-28 18:10:29 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1014,7 +1014,7 @@ function renderHTML(C) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DDNS Pro v4.2 - IP管理面板</title>
+    <title>DDNS Pro v4.3 - IP管理面板</title>
     <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>🌐</text></svg>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
